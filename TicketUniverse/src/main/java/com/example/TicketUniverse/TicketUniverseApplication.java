fc32572947038588af69ca909fa06dc9a00612c3
Gabriele <gabrieleingrassia3@gmail.com>
2023-10-24 10:48:00 +0200

    first commit

---
+++ TicketUniverse/src/main/java/com/example/TicketUniverse/TicketUniverseApplication.java
@@ -0,0 +1,13 @@
+package com.example.TicketUniverse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketUniverseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketUniverseApplication.class, args);
+	}
+
+}
---
+++ TicketUniverse/src/test/java/com/example/TicketUniverse/TicketUniverseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TicketUniverse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketUniverseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
