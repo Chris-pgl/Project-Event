a425d5e8cb518877350e8588416ba440fc838a55
Gabriele <gabrieleingrassia3@gmail.com>
2023-10-24 17:55:42 +0200

    creato metodo getAllUtenti

--- TicketUniverse/src/main/java/com/example/TicketUniverse/mapper/UtenteMapper.java
+++ TicketUniverse/src/main/java/com/example/TicketUniverse/mapper/UtenteMapper.java
@@ -1,10 +1,12 @@
 package com.example.TicketUniverse.mapper;
 
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
+
 
 import com.example.TicketUniverse.dto.UtenteDTO;
 import com.example.TicketUniverse.model.Utente;
+import org.springframework.context.annotation.Bean;
+
 
 @Mapper(componentModel = "spring")
 public interface UtenteMapper extends EntityMapper<UtenteDTO, Utente> {
@@ -12,4 +14,5 @@ public interface UtenteMapper extends EntityMapper<UtenteDTO, Utente> {
     UtenteDTO toDto(Utente utente);
 
     Utente toEntity(UtenteDTO utenteDTO);
+
 }
---
+++ TicketUniverse/src/main/java/com/example/TicketUniverse/service/UtenteService.java
@@ -0,0 +1,26 @@
+package com.example.TicketUniverse.service;
+
+import com.example.TicketUniverse.dto.UtenteDTO;
+import com.example.TicketUniverse.mapper.UtenteMapper;
+import com.example.TicketUniverse.model.Utente;
+import com.example.TicketUniverse.repositories.UtenteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class UtenteService {
+    @Autowired
+    private UtenteRepository utenteRepository;
+    @Autowired
+    private UtenteMapper utenteMapper;
+
+    public List<UtenteDTO> getAllUtenti(){
+        List<Utente> utenti = utenteRepository.findAll();
+        List<UtenteDTO> utentiDto = new ArrayList<>();
+        utenti.forEach(utente -> utentiDto.add(utenteMapper.toDto(utente)));
+        return utentiDto;
+    }
+}
